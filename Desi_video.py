43b0e0f3dec396e07474c39d3fec580abfa2e98c
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-05-03 14:53:15 +0530

    Update Desi_video.py

--- Desi_video.py
+++ Desi_video.py
@@ -72,6 +72,11 @@ API_LIST = [
     "https://you-pom-lover.vercel.app/xvideos/10/stepdaughter",
     "https://you-pom-lover.vercel.app/xvideos/10/sister",
     "https://you-pom-lover.vercel.app/xvideos/10/bhabhi"
+    "https://you-pom-lover.vercel.app/xvideos/10/mom"
+    "https://you-pom-lover.vercel.app/xvideos/10/dad"
+    "https://you-pom-lover.vercel.app/xvideos/10/stepdad"
+    "https://you-pom-lover.vercel.app/xvideos/10/stepmom"
+    "https://you-pom-lover.vercel.app/xvideos/10/stepsister"
 ]
 
 async def fetch_api_data(session, api_url):
@@ -190,7 +195,7 @@ async def auto_post():
 
                         buttons = InlineKeyboardMarkup([
                             [InlineKeyboardButton("📽️ Watch online", url=video_url)],
-                            [InlineKeyboardButton("📺 Join Our Channel", url="https://t.me/Opleech_WD")]
+                            [InlineKeyboardButton("📺 Join Our Channel", url="https://t.me/log_ak_bot")]
                         ])
 
                         try:
