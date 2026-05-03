bb601b39de434708036208f70a85c6dcab01b480
anujeditsbyanuj-bit <anujeditsbyanuj@gmail.com>
2026-05-03 14:48:27 +0530

    Update test_lol.py

--- test_lol.py
+++ test_lol.py
@@ -39,7 +39,7 @@ def get_random_headers():
 API_ID = int(os.environ.get("API_ID", 34446649))
 API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8735534617:AAFNBedzWsDRcsiw6GXBq7QAHbqJSJiDw0w")
-CHANNEL_ID = -1003951808679
+channel_id = os.environ.get("CHANNEL_ID", "-1003951808679")
 
 BLACKLIST_FILE = "blacklist.txt"
 
